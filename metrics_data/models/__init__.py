38645f4be97e35b7c5d9fd7e14ffa48c6dcb5275
Vladyslav Skrypchuk <51884701+Zmeykoo@users.noreply.github.com>
2022-01-16 23:17:27 +0200

    Delete metrics_data directory

--- metrics_data/models/__init__.py
+++
@@ -1,7 +0,0 @@
-from .user_models import (
-    User,
-    Token,
-    Project,
-    MetricsReportHistory
-)
-from .management import RiskScoreRules, RiskScoreMultiple
--- metrics_data/models/base.py
+++
@@ -1,3 +0,0 @@
-import sqlalchemy.ext.declarative
-
-Model = sqlalchemy.ext.declarative.declarative_base()
