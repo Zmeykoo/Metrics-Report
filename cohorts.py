3852b9f2426d884e917f3148fe2add19954f924d
Vladyslav Skrypchuk <51884701+Zmeykoo@users.noreply.github.com>
2022-01-27 13:44:01 +0200

    Add files via upload

--- cohorts.py
+++ cohorts.py
@@ -3,8 +3,9 @@ from metrics_data.metrics_report import MetricsReport
 class Cohorts(MetricsReport):
     """Cohorts"""
 
-    def initial_cohort(self, period):
+    def initial_cohort(self, dataframe, period):
         """Step 1"""
+
         p = None
         if period == 'month':
             p = 1
@@ -13,7 +14,14 @@ class Cohorts(MetricsReport):
         elif period == 'year':
             p = 12
 
-        customers_names = self.transformed_dataframe[self.date_fields[:p]].sum(axis=1)
+        customers_names = None
+        if dataframe == "ARR":
+            customers_names = self.transformed_dataframe[self.date_fields[:p]].sum(axis=1)
+
+        elif dataframe == "Gross Retained ARR":
+            customers_names = self.gross_retention_df[self.gross_retention_df.columns[:p]].sum(axis=1)
+
+
         tagged_customers = customers_names[customers_names.values > 0].index.tolist()
 
         print(f'Сustomer indexes that had greater than 0$ in the first {period}: {tagged_customers}\nnum = {len(tagged_customers)}')
@@ -22,11 +30,10 @@ class Cohorts(MetricsReport):
     def initial_cohort_arr(self, ic, period):
         """Step 2"""
         df = self.transformed_dataframe.filter(items=ic, axis=0)
-        #print(df)
+
 
         # sum by month
         if period == 'month':
-            print({k: self.transformed_dataframe[k].sum(axis=0) for k in self.date_fields})
             result_month = {k: df[k].sum(axis=0) for k in self.date_fields}
             return result_month
 
@@ -53,19 +60,26 @@ class Cohorts(MetricsReport):
         return result
 
 if __name__ == '__main__':
-    #fn = 'input.csv'
-    fn = 'orig.csv'
+    fn = 'input.csv'
+    #fn = 'orig.csv'
     report = Cohorts(fn)
-    initial_year = report.initial_cohort('year')
-    initial_month = report.initial_cohort('month')
-    ic_arr_month = report.initial_cohort_arr(initial_year, 'month')
-    ic_arr_quarter = report.initial_cohort_arr(initial_year, 'quarter')
-    ic_arr_year = report.initial_cohort_arr(initial_year, 'year')
-    print(ic_arr_month)
-    print(ic_arr_quarter)
-    print(ic_arr_year)
+
+    # ARR & Cohort Net Retention%
+    initial_year = report.initial_cohort('ARR', 'year')
+    initial_month = report.initial_cohort('ARR', 'month')
+
+    initial_gross_year = report.initial_cohort('Gross Retained ARR', 'year')
+    initial_gross_month = report.initial_cohort('Gross Retained ARR', 'month')
+
+    ic_arr_month = report.initial_cohort_arr(initial_gross_month, 'month')
+    ic_arr_quarter = report.initial_cohort_arr(initial_gross_month, 'quarter')
+    ic_arr_year = report.initial_cohort_arr(initial_gross_month, 'year')
+
     cohort_net = report.cohort_net_retention(ic_arr_month)
     cohort_net_quarter = report.cohort_net_retention(ic_arr_quarter)
     cohort_net_year = report.cohort_net_retention(ic_arr_year)
 
-
+    # Gross Retained ARR & Cohort Gross Retention %
+    #initial_gross_month = report.initial_cohort('month')
+    # Number of Customers
+    # Cohort Gross Logo Retention
