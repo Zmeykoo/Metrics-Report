a27df5e3204e3b6b6890f4708c76169df0cfd00b
Vladyslav Skrypchuk <skrypaslav@gmail.com>
2023-10-27 14:47:49 +0300

    Update new_cohort.py

--- new_cohort.py
+++ new_cohort.py
@@ -4,64 +4,60 @@ from metrics_data.metrics_report import MetricsReport
 
 class Cohorts(MetricsReport):
     """Cohorts"""
-    def initial_cohort(self, year, cohort):
 
-        if year in self.years:
-            df_year = self.transformed_dataframe[[m for m in self.date_fields if m.split('/')[1] == str(year)]]
-            #print(df_year)
-            months = df_year.columns.tolist()
-            k = 0
-            result = {}
-            initial_indexes = []
+    def __init__(self, *args, **kwargs):
+        super(Cohorts, self).__init__(*args, **kwargs)
 
-            for init_month in months:
-                initial_month = [i for i in df_year[df_year[init_month].values > 0].index.tolist() if i not in initial_indexes]
-                result_for_month = []
-                if cohort == 'arr':
-                    for m in months[k:]:
-                        total_month = [i for i in df_year[df_year[m].values > 0].index.tolist() if i in initial_month]
-                        result_for_month.append(sum(total_month))
-
-                elif cohort == 'number':
-                    """
-                    print(f'initial month: {initial_month}\n'
-                          f'used indexes: {initial_indexes}')
-                    """
-                    for m in months[k:]:
-                        total_month = [i for i in df_year[df_year[m].values > 0].index.tolist() if i in initial_month]
-                        #print(f'{init_month} {m}: {total_month}')
-                        result_for_month.append(len(total_month))
-
-                initial_indexes += initial_month
-                result.update({init_month: result_for_month})
-                k += 1
-
-            cohort_df = pd.DataFrame.from_dict(result, orient='index', columns=months)
-
-            return cohort_df
-
-        else:
+    def initial_cohort(self, year, cohort_type='arr'):
+        if year not in self.years:
             print(f'FY {year} was not defined.')
+            return None
 
-    def cohort_retention(self, ic):
-        """month / initial month * 100"""
-        def cohort_reteintion_data(ser):
-            res = round(ser / ic.iloc[:, 0] * 100, 2)
-            return res
+        df_year = self.transformed_dataframe[self.transformed_dataframe['Fiscal Year'] == year]
 
-        c_retention = ic.apply(cohort_reteintion_data, axis=0)
+        result = {}
+        initial_indexes = set()
 
+        months = self.date_fields
+        k = 0
 
-        print(f'Cohort Number of Customer:\n{ic}')
+        for init_month in months:
+            initial_month_data = df_year[df_year[init_month] > 0]
+
+            if cohort_type == 'arr':
+                result_for_month = []
+                for m in months[k:]:
+                    total_month_data = initial_month_data[initial_month_data[m] > 0]
+                    result_for_month.append(total_month_data['ARR'].sum())
+                result.update({init_month: result_for_month})
+
+            elif cohort_type == 'number':
+                result_for_month = []
+                for m in months[k:]:
+                    total_month_data = initial_month_data[initial_month_data[m] > 0]
+                    result_for_month.append(len(total_month_data))
+                result.update({init_month: result_for_month})
+
+            initial_indexes.update(initial_month_data.index)
+            k += 1
+
+        cohort_df = pd.DataFrame.from_dict(result, orient='index', columns=months)
+        return cohort_df
+
+    def cohort_retention(self, initial_cohort):
+        def cohort_retention_data(ser):
+            return round(ser / initial_cohort.iloc[:, 0] * 100, 2)
+
+        c_retention = initial_cohort.apply(cohort_retention_data, axis=0)
+
+        print(f'Cohort Number of Customers:\n{initial_cohort}')
         print(f'Cohort Retention:\n{c_retention}')
         return c_retention
 
-if __name__ == '__main__':
-    #fn = 'input.csv'
-    fn = 'cohort.csv'
-    #fn = 'orig.csv'
-    report = Cohorts(fn)
 
+if __name__ == '__main__':
+    fn = 'cohort.csv'
+    report = Cohorts(fn)
 
     initial_cohort_arr = report.initial_cohort(2018, 'arr')
     initial_cohort_number = report.initial_cohort(2018, 'number')
